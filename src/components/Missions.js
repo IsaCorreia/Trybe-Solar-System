4ebef1fc977d7fe0264a2f826136aaacd0fbd6a2
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-25 18:19:11 -0300

    Requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
+// Recomendação de uso para substituir o index no map. Créditos: Asafe Dainez (Turma XP - Tribo B)
+// Ref: https://robinpokorny.medium.com/index-as-a-key-is-an-anti-pattern-e0349aece318
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
+
+function rendersMissionsList() {
+  return missions.map((mission) => (<MissionCard
+    key={ nanoid() }
+    name={ mission.name }
+    year={ mission.year }
+    country={ mission.country }
+    destination={ mission.destination }
+  />));
+}
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {rendersMissionsList()}
       </div>
     );
   }
