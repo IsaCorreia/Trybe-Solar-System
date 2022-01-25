b6fa8c5ed24d4f9fbec15507d73a51a3b5a160da
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-25 17:54:33 -0300

    Requisito 5 - Refatoração: troca de index por nanoid()

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import PlanetCard from './PlanetCard';
+import { nanoid } from 'nanoid';
+// Recomendação de uso para substituir o index no map. Créditos: Asafe Dainez (Turma XP - Tribo B)
+// Ref: https://robinpokorny.medium.com/index-as-a-key-is-an-anti-pattern-e0349aece318
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 import Planets from '../data/planets';
 
 function rendersPlanetsList() {
-  return Planets.map((planet, i) => (<PlanetCard
-    key={ i }
+  return Planets.map((planet) => (<PlanetCard
+    key={ nanoid() }
     planetName={ planet.name }
     planetImage={ planet.image }
   />));
