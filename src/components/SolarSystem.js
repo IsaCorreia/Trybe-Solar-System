86a0a7b93cce2f52bbf1068e32d6e4fb4161c106
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-01-25 17:42:12 -0300

    Requisito 5

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import Planets from '../data/planets';
+
+function rendersPlanetsList() {
+  return Planets.map((planet, i) => (<PlanetCard
+    key={ i }
+    planetName={ planet.name }
+    planetImage={ planet.image }
+  />));
+}
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {rendersPlanetsList()}
       </div>
     );
   }
